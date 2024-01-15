57dcec21476d3a9a1129533f5ccc91d49e6d4226
Tommy Sanchez <tommy-s05@hotmail.com>
2024-01-15 18:24:55 -0400

    Clear unnecessary routes and view files

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+</script>
+
+<template>
+    <Head title="Home" />
+
+    Test
+</template>
+
+<style>
+
+</style>
